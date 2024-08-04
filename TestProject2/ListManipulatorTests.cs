bc0bc969344731c76da686d4d4f871bf56779610
Jalba Oleg <jalbaoleg00@gmail.com>
2024-08-04 16:45:56 +0300

    add test

---
+++ TestProject2/ListManipulatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+public class ListManipulatorTests
+{
+    [Fact]
+    public void FindMax_ListOfNumbers_ReturnsMaxValue()
+    {
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var result = ListManipulator.FindMax(numbers);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void RemoveDuplicates_ListWithDuplicates_ReturnsListWithoutDuplicates()
+    {
+        var numbers = new List<int> { 1, 2, 3, 3, 4, 5, 5 };
+        var result = ListManipulator.RemoveDuplicates(numbers);
+        var expected = new List<int> { 1, 2, 3, 4, 5 };
+        Assert.Equal(expected, result);
+    }
+}
---
+++ TestProject2/StringValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+public class StringValidatorTests
+{
+    [Fact]
+    public void IsValidEmail_ValidEmail_ReturnsTrue()
+    {
+        var result = StringValidator.IsValidEmail("test@example.com");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsValidEmail_InvalidEmail_ReturnsFalse()
+    {
+        var result = StringValidator.IsValidEmail("testexample.com");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsPhoneNumber_ValidPhoneNumber_ReturnsTrue()
+    {
+        var result = StringValidator.IsPhoneNumber("1234567890");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsPhoneNumber_InvalidPhoneNumber_ReturnsFalse()
+    {
+        var result = StringValidator.IsPhoneNumber("12345abcde");
+        Assert.False(result);
+    }
+}
